3a17a1fc04177191e8b1de81bd90d74f6575c9a2
Patategrill <194829329+patategrill@users.noreply.github.com>
2026-06-22 22:20:42 +0200

    Update kernel.c

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -8,12 +8,12 @@ void kernel_main() {
 
         int indice = 0;
 
-        for (int i = 0; i <= 36; i += 2){
+        for (int i = 0; i <= 40; i += 2){
             video_memory[i] = msg_bienvenue[indice];
             indice = indice + 1;
         }
 
-        for (int j = 1; j <= 36; j += 2){
+        for (int j = 1; j <= 40; j += 2){
             video_memory[j] = 0x7;
         }
     }
