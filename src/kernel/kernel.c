76c7e1c7177bf2c324f1f0cbfd3f5c6a66e7e315
Patategrill <194829329+patategrill@users.noreply.github.com>
2026-06-22 22:16:19 +0200

    Update kernel.c

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -6,14 +6,15 @@ void kernel_main() {
 
         char msg_bienvenue[] = "Welcome on MownOS!\n";
 
-        int len = strlen(msg_bienvenue);
+        int indice = 0;
 
-        for (int i = 0; i <= len; i += 2){
-            video_memory[i] = msg_bienvenue[len];
+        for (int i = 0; i <= 36; i += 2){
+            video_memory[i] = msg_bienvenue[indice];
+            indice = indice + 1;
         }
 
-        for (int j = 1; j <=len; j += 2){
-            video_memory[j] = 0x8;
+        for (int j = 1; j <= 36; j += 2){
+            video_memory[j] = 0x7;
         }
     }
 }
