32474ce9f31ffab6a95637e7c7c5fb31de05c865
Patategrill <194829329+patategrill@users.noreply.github.com>
2026-06-23 20:51:41 +0200

    Update kernel.c

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 
 void kernel_main() {
-    while (1) {
-        char* video_memory = (char*) 0xb8000;
+    char* video_memory = (char*) 0xb8000;
 
-        char msg_bienvenue[] = "Welcome on MownOS!\n";
+    char msg_bienvenue[] = "Welcome on MownOS!\n";
+    
+    int indice = 0;
 
-        int indice = 0;
+    for (int i = 0; i <= 40; i += 2){
+        video_memory[i] = msg_bienvenue[indice];
+        indice = indice + 1;
+    }
 
-        for (int i = 0; i <= 40; i += 2){
-            video_memory[i] = msg_bienvenue[indice];
-            indice = indice + 1;
-        }
+    for (int j = 1; j <= 40; j += 2){
+        video_memory[j] = 0x7;
+    }
 
-        for (int j = 1; j <= 40; j += 2){
-            video_memory[j] = 0x7;
-        }
+    char tty[] = "MownOS >";
+    indice = 0;
+
+    for (int i = 0; i <= 16; i += 2){
+        video_memory[i] = tty[indice];
+        indice = indice + 1;
+    }
+
+    for (int j = 1; j <= 16; j += 2){
+        video_memory[j] = 0x7;
     }
 }
